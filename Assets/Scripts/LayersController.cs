978a46f81b5a602c4deb075c5beef377e67d5bcf
Joao Pedro <j.p.a.caixeta@gmail.com>
2020-04-30 22:30:59 -0300

    Project Upload

---
+++ Assets/Scripts/LayersController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LayersController : MonoBehaviour
+{
+    public GameObject blocos;
+    public GameObject blocosWF;
+    public GameObject blocosintersect;
+    public Slider slider;
+
+
+    void Start()
+    {
+
+        foreach (Transform child in blocos.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        blocos.transform.Find("1").gameObject.SetActive(true);
+
+        foreach (Transform child in blocosWF.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        blocosWF.transform.Find("1").gameObject.SetActive(true);
+
+        foreach (Transform child in blocosintersect.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+    }
+
+    void OnGUI()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            slider.value -= 1;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            slider.value += 1;
+        }
+    }
+
+
+
+    public void ShowLayer(float value)
+    {
+        foreach (Transform child in blocos.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+        foreach (Transform child in blocosWF.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+        foreach (Transform child in blocosintersect.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+        blocos.transform.Find(value.ToString()).gameObject.SetActive(true);
+        blocosWF.transform.Find(value.ToString()).gameObject.SetActive(true);
+        blocosintersect.transform.Find((value-1f).ToString()).gameObject.SetActive(true);
+        
+    }
+}
---
+++ Assets/Scripts/ShowValue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowValue : MonoBehaviour
+{
+    public Text txt;
+  
+    void Start()
+    {
+        txt = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    public void TextUpdate (float value)
+    {
+        txt.text = value.ToString();
+    }
+}
